486c29979b441d5b7006fe42104f466655065363
izak <isaachizekiel@gmail.com>
2022-10-29 23:52:01 +0300

    still context is null

--- android/app/src/main/jni/main.c
+++ android/app/src/main/jni/main.c
@@ -17,11 +17,11 @@ struct saved_state {
 };
 
 // global pointers are dangerous but let's take our chances
-struct es_context * context = {0};
+// struct es_context * context = {0};
 
 // all application lifecycle callbacks are handled here
 static void handle_cmd(struct android_app* app, int32_t cmd) {
-    context = (struct es_context *) app->userData;
+    struct es_context *context = ( struct es_context *) app->userData;
     switch (cmd) {
         case APP_CMD_INIT_WINDOW:
             LOGI("APP_CMD_INIT_WINDOW: %d", cmd);
@@ -39,11 +39,11 @@ static void handle_cmd(struct android_app* app, int32_t cmd) {
             LOGI("APP_CMD_CONTENT_RECT_CHANGED: %d", cmd);
             break;
         case APP_CMD_GAINED_FOCUS:
-            context->animating = 1;
+            ///context->animating = 1;
             LOGI("APP_CMD_GAINED_FOCUS: %d", cmd);
             break;
         case APP_CMD_LOST_FOCUS:
-            context->animating = 0;
+            //context->animating = 0;
             LOGI("APP_CMD_LOST_FOCUS: %d", cmd);
             break;
         case APP_CMD_CONFIG_CHANGED:
@@ -100,7 +100,7 @@ static void do_frame() {
     // LOGI("doing frame");
 }
 
-static void game_loop() {
+static void game_loop(struct es_context * context) {
     while(1) {
         int events;
         struct android_poll_source* source;
@@ -111,6 +111,7 @@ static void game_loop() {
                 source->process(context->platform_data, source);
             }
             // are always exiting
+
             if (((struct android_app *)context->platform_data)->destroyRequested) return;
         }
         if (context->animating) do_frame();
@@ -121,12 +122,12 @@ void android_main(struct android_app* app) {
     app->onAppCmd = handle_cmd;
     app->onInputEvent = handle_input;
 
-    context = malloc (sizeof (struct es_context));
+    struct es_context *context = malloc (sizeof (struct es_context));
 
     context->platform_data = app;
 
     // main window loop
-    game_loop();
+    game_loop(context);
 
     // release the resource used by the window
     free(context);

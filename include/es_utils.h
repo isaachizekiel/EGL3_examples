aa397856fcbc53f3d227a0956f629a2f8a407443
izak <isaachizekiel@gmail.com>
2022-10-30 23:40:56 +0300

    refactor

--- include/es_utils.h
+++ include/es_utils.h
@@ -7,6 +7,26 @@
 #include <EGL/egl.h>
 #include <EGL/eglplatform.h>
 
+#ifdef ANDROID
+#include <android/log.h>
+#include <android_native_app_glue.h>
+#endif
+
+#define DEBUG_TAG "EGL3_examples"
+
+#ifdef ANDROID
+#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, __VA_ARGS__))
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, DEBUG_TAG, __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, DEBUG_TAG, __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, DEBUG_TAG, __VA_ARGS__))
+#define ABORT_GAME { LOGE("*** GAME ABORTING."); *((volatile char*)0) = 'a'; }
+#else
+#define LOGD(...) ((void)fprintf(stdout, __VA_ARGS__))
+#define LOGI(...) ((void)fprintf(stdout, __VA_ARGS__))
+#define LOGW(...) ((void)fprintf(stdout, __VA_ARGS__))
+#define LOGE(...) ((void)fprintf(stderr, __VA_ARGS__))
+#define ABORT_GAME { LOGE("*** GAME ABORTING."); *((volatile char*)0) = 'a'; }
+#endif
 
 
 #define ES_WINDOW_RGB 0
--- src/es_utils.c
+++ src/es_utils.c
@@ -4,27 +4,6 @@
 #include <stdlib.h>
 #include "es_utils.h"
 
-#ifdef ANDROID
-#include <android/log.h>
-#include <android_native_app_glue.h>
-#endif
-
-#define DEBUG_TAG "EGL3_examples"
-
-#ifdef ANDROID
-#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, __VA_ARGS__))
-#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, DEBUG_TAG, __VA_ARGS__))
-#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, DEBUG_TAG, __VA_ARGS__))
-#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, DEBUG_TAG, __VA_ARGS__))
-#define ABORT_GAME { LOGE("*** GAME ABORTING."); *((volatile char*)0) = 'a'; }
-#else
-#define LOGD(...) ((void)fprintf(stdout, __VA_ARGS__))
-#define LOGI(...) ((void)fprintf(stdout, __VA_ARGS__))
-#define LOGW(...) ((void)fprintf(stdout, __VA_ARGS__))
-#define LOGE(...) ((void)fprintf(stderr, __VA_ARGS__))
-#define ABORT_GAME { LOGE("*** GAME ABORTING."); *((volatile char*)0) = 'a'; }
-#endif
-    
 
 static EGLint get_context_renderable_type(EGLDisplay egl_display) {
 #ifdef EGL_KHR_create_context
